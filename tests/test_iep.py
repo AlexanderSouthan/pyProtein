d529ed29ff98ae6a7946887a40c5539ef064fa8e
Snijderfrey <peter.vandersnijderfrey@web.de>
2021-10-16 20:08:03 +0200

    Added first test example

---
+++ tests/test_iep.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 16 19:59:07 2021
+
+@author: almami
+"""
+
+import unittest
+
+from pyPolyampholyte.polyampholyte import polyampholyte
+
+class TestIEP(unittest.TestCase):
+
+    def test_iep(self):
+        """Test IEP calculation"""
+
+        # Define bovine serum albumin via its amino acid sequence.
+        # DTHKSEIAHRFKDLGEEHFKGLVLIAFSQYLQQCPFDEHVKLVNELTEFAKTCVADESHAGCEKSLHTLFGDELCKVASLRETYGDMADCCEKQEPERNECFLSHKDDSPDLPKLKPDPNTLCDEFKADEKKFWGKYLYEIARRHPYFYAPELLYYANKYNGVFQECCQAEDKGACLLPKIETMREKVLASSARQRLRCASIQKFGERALKAWSVARLSQKFPKAEFVEVTKLVTDLTKVHKECCHGDLLECADDRADLAKYICDNQDTISSKLKECCDKPLLEKSHCIAEVEKDAIPENLPPLTADFAEDKDVCKNYQEAKDAFLGSFLYEYSRRHPEYAVSVLLRLAKEYEATLEECCAKDDPHACYSTVFDKLKHLVDEPQNLIKQNCDQFEKLGEYGFQNALIVRYTRKVPQVSTPTLVEVSRSLGKVGTRCCTKPESERMPCTEDYLSLILNRLCVLHEKTPVSEKVTKCCTESLVNRRPCFSALTPDETYVPKAFDEKLFTFHADICTLPDTEKQIKKQTALVELLKHKPKATEEQLKTVMENFVAFVDKCCAADDKEACFAVEGPKLVVSTQTALA
+        bovine_serum_albumin_mature = (
+                'DTHKSEIAHRFKDLGEEHFKGLVLI'
+                'AFSQYLQQCPFDEHVKLVNELTEFAKTCVADESHAGCEKSLHTLFGDEL'
+                'CKVASLRETYGDMADCCEKQEPERNECFLSHKDDSPDLPKLKPDPNTLC'
+                'DEFKADEKKFWGKYLYEIARRHPYFYAPELLYYANKYNGVFQECCQAED'
+                'KGACLLPKIETMREKVLASSARQRLRCASIQKFGERALKAWSVARLSQK'
+                'FPKAEFVEVTKLVTDLTKVHKECCHGDLLECADDRADLAKYICDNQDTI'
+                'SSKLKECCDKPLLEKSHCIAEVEKDAIPENLPPLTADFAEDKDVCKNYQ'
+                'EAKDAFLGSFLYEYSRRHPEYAVSVLLRLAKEYEATLEECCAKDDPHAC'
+                'YSTVFDKLKHLVDEPQNLIKQNCDQFEKLGEYGFQNALIVRYTRKVPQV'
+                'STPTLVEVSRSLGKVGTRCCTKPESERMPCTEDYLSLILNRLCVLHEKT'
+                'PVSEKVTKCCTESLVNRRPCFSALTPDETYVPKAFDEKLFTFHADICTL'
+                'PDTEKQIKKQTALVELLKHKPKATEEQLKTVMENFVAFVDKCCAADDKE'
+                'ACFAVEGPKLVVSTQTALA')
+        bovine_serum_albumin = polyampholyte(
+                'protein', sequence=bovine_serum_albumin_mature,
+                pka_data='pka_bjellqvist', mod_types=['N_term', 'C_term'],
+                mod_abundances=[1, 1],
+                pka_scales=['pka_bjellqvist', 'pka_bjellqvist'])
+
+        bovine_serum_albumin.IEP()
+
+        # test_input = [13, 2, 5, 7]
+        # test_output = sum(probabilities(test_input))
+        # expected_output = 1
+        # error_message =  "Should sum to 1"
+        # self.assertEqual(test_output, expected_output, error_message)
