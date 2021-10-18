84be4a8dbe081099f5c6e816aab976b4d140f0c4
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2021-10-18 16:04:32 +0200

    Added dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='pyProtein',
     version='0.0.2',
-    packages=find_packages(where='src')
+    packages=find_packages(where='src'),
+    install_requires=['numpy', 'pandas', 'scipy']
 )
