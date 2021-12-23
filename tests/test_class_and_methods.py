868e26a6d8fa7cfa863fe510e60e6fb5d904cd44
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2021-12-23 20:29:42 +0100

    Added missing tests

--- tests/test_class_and_methods.py
+++ tests/test_class_and_methods.py
@@ -7,9 +7,11 @@ Created on Sat Oct 16 19:59:07 2021
 """
 
 import numpy as np
+import pandas as pd
 import unittest
 
 from src.pyProtein import protein
+from src.pyProtein import amino_acids
 
 
 class TestInitialize(unittest.TestCase):
@@ -56,6 +58,9 @@ class TestInitialize(unittest.TestCase):
         self.assertAlmostEqual(bsa_iep_emboss, 5.6662233, 4)
         self.assertAlmostEqual(bsa_iep_ipc_protein, 5.40694206, 4)
 
+        IEP_nan = bsa.IEP(ph_range=[0, 1])
+        self.assertTrue(np.isnan(IEP_nan))
+
         # Charge curve calculation test in the following.
         ph_range = [3, 12]
         data_points = 121
@@ -96,6 +101,12 @@ class TestInitialize(unittest.TestCase):
         self.assertAlmostEqual(bsa_n_main_chain, 0.16471258, 4)
         self.assertAlmostEqual(bsa_n_end_groups, 0, 4)
 
+        # test errors
+        self.assertRaises(ValueError, protein, 'abs', [])
+        self.assertRaises(ValueError, bsa.molar_mass, 'al')
+        self.assertRaises(ValueError, bsa.mean_residue_molar_mass, 'al')
+        self.assertRaises(ValueError, bsa.n_content, 'al')
+
     def test_mode_mmol_g(self):
 
         # test with a list as input
@@ -112,3 +123,65 @@ class TestInitialize(unittest.TestCase):
         gelatin_type_a_porcine_np = np.array(gelatin_type_a_porcine)
         gel_a_np = protein(
             'mmol_g', gelatin_type_a_porcine_np)
+
+        # test errors
+        self.assertRaises(Exception, gel_a.molar_mass)
+
+    def test_absolute(self):
+        col_1_a1_sequence = (
+            'QLSYGYDEKSTGGISVPGPMGPSGPRGLPGPPGAPGPQGFQGPPGEPGEPGASGPMGPRGPPGP'
+            'PGKNGDDGEAGKPGRPGERGPPGPQGARGLPGTAGLPGMKGHRGFSGLDGAKGDAGPAGPKGEP'
+            'GSPGENGAPGQMGPRGLPGERGRPGAPGPAGARGNDGATGAAGPPGPTGPAGPPGFPGAVGAKG'
+            'EAGPQGPRGSEGPQGVRGEPGPPGPAGAAGPAGNPGADGQPGAKGANGAPGIAGAPGFPGARGP'
+            'SGPQGPGGPPGPKGNSGEPGAPGSKGDTGAKGEPGPVGVQGPPGPAGEEGKRGARGEPGPTGLP'
+            'GPPGERGGPGSRGFPGADGVAGPKGPAGERGSPGPAGPKGSPGEAGRPGEAGLPGAKGLTGSPG'
+            'SPGPDGKTGPPGPAGQDGRPGPPGPPGARGQAGVMGFPGPKGAAGEPGKAGERGVPGPPGAVGP'
+            'AGKDGEAGAQGPPGPAGPAGERGEQGPAGSPGFQGLPGPAGPPGEAGKPGEQGVPGDLGAPGPS'
+            'GARGERGFPGERGVQGPPGPAGPRGANGAPGNDGAKGDAGAPGAPGSQGAPGLQGMPGERGAAG'
+            'LPGPKGDRGDAGPKGADGSPGKDGVRGLTGPIGPPGPAGAPGDKGESGPSGPAGPTGARGAPGD'
+            'RGEPGPPGPAGFAGPPGADGQPGAKGEPGDAGAKGDAGPPGPAGPAGPPGPIGNVGAPGAKGAR'
+            'GSAGPPGATGFPGAAGRVGPPGPSGNAGPPGPPGPAGKEGGKGPRGETGPAGRPGEVGPPGPPG'
+            'PAGEKGSPGADGPAGAPGTPGPQGIAGQRGVVGLPGQRGERGFPGLPGPSGEPGKQGPSGASGE'
+            'RGPPGPMGPPGLAGPPGESGREGAPGAEGSPGRDGSPGAKGDRGETGPAGPPGAPGAPGAPGPV'
+            'GPAGKSGDRGETGPAGPTGPVGPVGARGPAGPQGPRGDKGETGEQGDRGIKGHRGFSGLQGPPG'
+            'PPGSPGEQGPSGASGPAGPRGPPGSAGAPGKDGLNGLPGPIGPPGPRGRTGDAGPVGPPGPPGP'
+            'PGPPGPPSAGFDFSFLPQPPQEKAHDGGRYYRA')
+        col_1_a1_abundances = analyze_sequence(col_1_a1_sequence)
+        col_1_a1_abundances['K'] -= 10
+        col_1_a1_abundances['P'] -= 111
+        col_1_a1_abundances['Hyl'] += 10
+        col_1_a1_abundances['Hyp'] += 111
+
+        # Collagen type I human uniprot.org
+        col_1_a1 = protein(
+            'absolute', col_1_a1_abundances.to_list(),
+            pka_data='pka_bjellqvist', mod_types=['N_term', 'C_term'],
+            mod_abundances=[1, 1])
+
+        col_mm = col_1_a1.molar_mass()
+
+    def test_res_per_1000(self):
+        gel_a_wt_perc = np.array([
+            5.91, 0, 1.72, 3.45, 10.50, 0, 24.89, 9.24, 0, 2.42, 0.90, 1.23,
+            2.92, 0.60, 2.08, 0.75, 3.79, 8.42, 13.58, 0, 11.82, 1.31])
+        gel_a_mmol_g = gel_a_wt_perc/amino_acids['molar_mass']*10
+        gel_a_mmol_g_norm = (100 * gel_a_mmol_g / np.sum(gel_a_mmol_g))
+        gel_a_per_1000 = gel_a_mmol_g_norm * 10
+
+        gel_a = protein('res_per_1000', gel_a_per_1000.values)
+        print(gel_a.mean_residue_molar_mass())
+
+        # test errors
+        self.assertRaises(Exception, gel_a.molar_mass)
+
+def analyze_sequence(sequence):
+    abundance = []
+    # occurences of the first 20 amino acids in the sequence are
+    # counted, needs to be adapted if more than 20 amino acids such
+    # as hydroxylysine are added
+    for key in amino_acids.index[:20]:
+        abundance.append(sequence.count(key))
+    # Hyp, Hyl abundances
+    abundance.extend(
+        (len(amino_acids.index) - len(abundance)) * [0])
+    return pd.Series(abundance, index=amino_acids.index)
