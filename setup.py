d0370a5e40fa4e7ef416d880968b416a8ad2e6fb
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2026-07-24 11:23:04 +0200

    Changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyProtein',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(where='src'),
     install_requires=['numpy', 'pandas', 'scipy']
 )
